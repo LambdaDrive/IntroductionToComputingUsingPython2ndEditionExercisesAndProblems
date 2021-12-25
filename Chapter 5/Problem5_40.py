944db6a6f309978b61f6738d492b81dcbc1ce064
Huriel Romanzini <chuva_de_pedra@hotmail.com>
2021-12-25 17:05:42 -0300

    fixed solution to problem 40 from chapter 5

--- Chapter 5/Problem5_40.py
+++ Chapter 5/Problem5_40.py
@@ -1,33 +1,18 @@
 def approxPi(error):
-    divup = 4
-    div = 1
-    div1 =1
-    soma1 = 0
-    soma2 = 0
+    constant = 4
+    mutable = 1
     step = 1
-    step1 = 1
-    times = 1
-    while soma1 - soma2 != error:
-        for i in range(times - 1):
-            if step1 % 2 == 0:
-                soma2 -= 4/div1
-            else:
-                soma2 += 4/div1
-            div1 += 2
-            step1 += 1
-        for i in range(times):
-            if step % 2 == 0:
-                soma1 -= 4/div
-            else:
-                soma1 += 4/div
-            div += 2
-            step += 1
-        times += 1
-        div1 = 1
-        div = 1
-        step1 = 1
-        step = 1
-        if abs(soma1 - soma2) <= error:
-            return soma1
-        soma1 = 0
-        soma2 = 0
+    soma = 0
+    prev = 0
+    while True:
+        if step % 2 != 0:
+            soma+= constant/mutable
+        else:
+            soma -= constant/mutable
+        if soma > 0 and prev > 0:
+            if abs(soma - prev) < error:
+                return soma
+        prev = soma
+        step += 1
+        mutable += 2
+        
