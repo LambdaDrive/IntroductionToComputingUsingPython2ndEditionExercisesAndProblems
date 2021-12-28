8e20a60e33713547dd9a2f11f24d510461b5a6be
Huriel Romanzini <chuva_de_pedra@hotmail.com>
2021-12-28 15:09:06 -0300

    solved first exercise of chapter 6

--- Chapter 6/exercises/Exercise6_11.py
+++ Chapter 6/exercises/Exercise6_11.py
@@ -1,10 +1,17 @@
 def easyCrypto(string):
     letters = 'abcdefghijklmnopqrstuvwxyz'
     cry = {}
-    for i in range(len(letters)-1):
-        if i % 2 != 0:
+    result = ''
+    for i in range(len(letters)-1):   
+        if i % 2 == 0:
             cry[letters[i]] = letters[i+1]
         else:
             cry[letters[i]] = letters[i-1]
-    print(cry)
+        cry['z'] = 'y'
+    for letter in string:
+        if letter.isupper():
+            result += cry[letter.lower()].capitalize()
+        else:
+            result += cry[letter]
+    print(result)
     
